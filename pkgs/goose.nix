84a35c1a561018548d9a4166559aef228e6ce7f3
longranger <longranger.406@gmail.com>
2026-08-01 09:51:14 -0700

    Add llvmPackages build dependencies.

--- pkgs/goose.nix
+++ pkgs/goose.nix
@@ -7,16 +7,14 @@
   openssl,
   dbus,
   stdenv,
+  llvmPackages,
 }:
 
 let
   version = "1.45.0"; # renovate: datasource=github-releases depName=aaif-goose/goose
 
-  # Fetch the static rusty_v8 library explicitly for the build sandbox
-  # Matches the version requested in the build log (v145.0.0)
   rusty_v8_lib = fetchurl {
     url = "https://github.com/denoland/rusty_v8/releases/download/v145.0.0/librusty_v8_release_${stdenv.hostPlatform.config}.a.gz";
-    # Use a dummy hash first if you want Nix to verify the SRI hash
     hash = "sha256-chV1PAx40UH3Ute5k3lLrgfhih39Rm3KqE+mTna6ysE=";
   };
 in
@@ -41,11 +39,23 @@ rustPlatform.buildRustPackage rec {
   # Pass the fetched static lib to rusty_v8's build script
   RUSTY_V8_ARCHIVE = rusty_v8_lib;
 
-  nativeBuildInputs = [ pkg-config ];
+  nativeBuildInputs = [
+    pkg-config
+    llvmPackages.clang
+  ];
+
   buildInputs = [
     openssl
+    llvmPackages.libclang.lib
     dbus
   ];
+
+  env = {
+    RUSTY_V8_ARCHIVE = rusty_v8_lib;
+    LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
+  };
+
+  BINDGEN_EXTRA_CLANG_ARGS = "-isystem ${llvmPackages.libclang.lib}/lib/clang/${llvmPackages.libclang.version}/include";
 
   meta = with lib; {
     description = "Block Goose AI developer agent CLI";
